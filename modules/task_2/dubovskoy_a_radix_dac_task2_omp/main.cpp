710bd0c8e53683cb1a7aeb09696546134cc768e5
DubovskoyAndrey <32214387+DubovskoyAndrey@users.noreply.github.com>
2019-04-19 17:06:25 +0300

    Update main.cpp

--- modules/task_2/dubovskoy_a_radix_dac_task2_omp/main.cpp
+++ modules/task_2/dubovskoy_a_radix_dac_task2_omp/main.cpp
@@ -125,8 +125,7 @@ void dac_sort(unsigned int * array, int size, int threads) {
     printf("%d ", i);
     radix_sort(array + i * piece, piece_mas[i]);
   }
-  print_array(array, size);
-  printf("4\n");
+  printf("\n 4\n");
 
   int counter = static_cast<int>(std::log(threads) / std::log(2));
   printf(" Counter = %d \n", counter);
